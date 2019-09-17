c06cae45239b8f3dabf144e84f74bb7a2fd3975d
Robert Jördens <rj@quartiq.de>
2019-09-17 15:09:43 +0000

    fastino [wip]

--- artiq/gateware/rtio/phy/fastino.py
+++ artiq/gateware/rtio/phy/fastino.py
@@ -1,11 +1,23 @@
 from migen import *
 from migen.genlib.cdc import MultiReg
-from migen.genlib.io import DifferentialOutput, DifferentialInput
+from migen.genlib.io import DifferentialOutput, DifferentialInput, DDROutput
+from misoc.cores.liteeth_mini.mac.crc import LiteEthMACCRCEngine
 
 from artiq.gateware.rtio import rtlink
 
 
 class SerDes(Module):
+    def transpose(self, i, n):
+        # i is n,m c-contiguous
+        # o is m,n c-contiguous
+        o = Signal.like(i)
+        m = len(i)//n
+        assert n*m == len(i)
+        for nn in range(n):
+            for mm in range(m):
+                self.comb += o[mm*n + nn].eq(i[nn*m + mm])
+        return o
+
     def __init__(self, pins, pins_n):
         n_bits = 16
         n_channels = 32
@@ -14,27 +26,30 @@ class SerDes(Module):
         n_frame = 14
         n_lanes = len(pins.mosi)
 
-        self.dac = [Signal(n_bits) for i in range(n_channels)]
-        self.mask = Signal(n_channels)
-        self.cfg = Signal(16)
+        self.dac = [Signal(n_bits, reset=i) for i in range(n_channels)]
+        self.mask = Signal(n_channels, reset=(1 << n_channels) - 1)
+        self.cfg = Signal(16, reset=0)
         adr = Signal(4)
         self.dat_r = Signal(n_frame*(1 << len(adr)))
-        crc = Signal(16)
 
-        self.stb = Signal()
-        clk = Signal(n_div, reset=0b1100011)
-        i_frame = Signal(max=n_frame)
+        checksum = Signal(16)
+        crc = LiteEthMACCRCEngine(
+            data_width=2*n_lanes, width=len(checksum), polynom=0x1021)
+        self.submodules += crc
+
         n_word = n_lanes*n_div
-        body = Signal(n_frame*n_word - n_frame//2 - 1 - len(crc))
+        body = Signal(n_frame*n_word - n_frame//2 - 1 - len(checksum))
         body_ = Cat(self.cfg, adr, self.dac, self.mask)
         assert len(body) == len(body_)
+        self.comb += body.eq(body_)
+
         words = Signal(n_frame*n_word)
         words_ = []
         j = 0
         for i in range(n_frame):
             if i == 0:
                 words_.append(C(1))
-                words_.append(crc)
+                words_.append(checksum)
             elif i <= n_frame//2:
                 words_.append(C(0))
             k = n_word - (len(Cat(words_)) % n_word)
@@ -42,37 +57,47 @@ class SerDes(Module):
             j += k
         words_ = Cat(words_)
         assert len(words_) == len(words)
+        self.comb += words.eq(words_)
+
+        self.stb = Signal()
+        clk = Signal(n_div, reset=0b1100011)
         stb_clk = Signal()
+        i_frame = Signal(max=n_frame//2)
         self.comb += [
-            stb_clk.eq(clk[1:3] == 0b10),
-            self.stb.eq(stb_clk & (i_frame == n_frame - 1)),
-            words.eq(words_),
-            body.eq(body_),
+            stb_clk.eq(~clk[0] & clk[-1]),
+            self.stb.eq(stb_clk & (i_frame == n_frame//2 - 1)),
         ]
         sr = [Signal(n_frame*n_div, reset_less=True) for i in range(n_lanes)]
         miso = Signal()
-        miso_sr = Signal(n_div, reset_less=True)
+        miso_sr = Signal(n_frame, reset_less=True)
         self.sync.rio_phy += [
-            clk.eq(Cat(clk[-1], clk)),
-            [sri[1:].eq(sri) for sri in sr],
+            clk.eq(Cat(clk[-2:], clk)),
+            [sri[2:].eq(sri) for sri in sr],
+            If(~clk[0],
+                miso_sr.eq(Cat(miso, miso_sr)),
+            ),
             If(stb_clk,
                 i_frame.eq(i_frame + 1),
-                miso_sr.eq(Cat(miso, miso_sr)),
             ),
             If(self.stb,
                 i_frame.eq(0),
-                Cat(sr).eq(words),
+                Cat(sr).eq(self.transpose(words, n_frame*n_div)),
                 adr.eq(adr + 1),
                 Array([self.dat_r[i*n_frame:(i + 1)*n_frame]
                     for i in range(1 << len(adr))])[adr].eq(miso_sr),
             )
         ]
 
+        clk_ddr = Signal()
+        miso0 = Signal()
         self.specials += [
-                DifferentialOutput(clk[-1], pins.clk, pins_n.clk),
-                [DifferentialOutput(sri[-1], mp, mn) for sri, mp, mn in zip(sr,
-                    pins.mosi, pins_n.mosi)],
-                DifferentialInput(pins.miso, pins_n.miso, miso),
+                DDROutput(clk[-1], clk[-2], clk_ddr, ClockSignal("rio_phy")),
+                DifferentialOutput(clk_ddr, pins.clk, pins_n.clk),
+                [(DDROutput(sri[-1], sri[-2], ddr, ClockSignal("rio_phy")),
+                  DifferentialOutput(ddr, mp, mn))
+                  for sri, ddr, mp, mn in zip(sr, Signal(n_lanes), pins.mosi, pins_n.mosi)],
+                DifferentialInput(pins.miso, pins_n.miso, miso0),
+                MultiReg(miso0, miso, "rio_phy"),
         ]
 
 
@@ -87,10 +112,13 @@ class Fastino(Module):
         self.sync.rtio += [
                 If(self.rtlink.o.stb,
                     Array(self.serializer.dac + [
-                        self.serializer.mask[0],
-                        self.serializer.mask[1],
+                        self.serializer.mask[:16],
+                        self.serializer.mask[16:],
                         self.serializer.cfg
                         ])[self.rtlink.o.address].eq(self.rtlink.o.data)
                 )
+                self.rtlink.i.stb.eq(self.rtlink.o.stb & (self.rtlink.o.address[2:5] == 0b111100)),
+                self.rtlink.i.data.eq(Array([
+                    self.serializer.dat_r[i*16:(i + 1)*16] for i in range(4)])[
+                        self.rtlink.o.address[:2]]),
         ]
-        #self.specials += MultiReg(roi_boundary, target, "cl")
